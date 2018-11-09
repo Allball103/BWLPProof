332a73c5a6a88cc4069e34e59d2ed679878cae4c
Phil Lavoie <philip.lavoie@uvm.edu>
2018-11-09 14:48:13 -0500

    added in javafx to EventLoop with just a random example...it works

--- EventLoop.java
+++ EventLoop.java
@@ -1,7 +1,30 @@
-// Defines EventLoop class
-public class EventLoop{
-    public static void main(String args[]){
+package sample;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class EventLoop extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("HBox Experiment 1");
+
+        TextField textField = new TextField();
+
+        HBox hbox = new HBox(textField);
+
+        Scene scene = new Scene(hbox, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
